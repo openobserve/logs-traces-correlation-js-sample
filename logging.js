c6ad1fa27532a1eb0a127a983526a5f7db9e7df3
omkark06 <omkar@zinclabs.io>
2024-12-13 17:13:13 +0530

    updated pino and tracing packages

--- logging.js
+++ logging.js
@@ -1,35 +1,34 @@
-const pino = require("pino");
-const pinoOpenObserve = require('@openobserve/pino-openobserve')
+const pino = require('pino');
 const { trace, context } = require('@opentelemetry/api');
+const OpenobserveTransport = require('@openobserve/pino-openobserve');
 
 const logger = pino({
-  level: "info",
-   formatters: {
-        log: (object) => {
-            const spanContext = trace.getSpan(context.active());
-            if (spanContext) {
-              object.traceId = spanContext.spanContext().traceId;
-              object.spanId = spanContext.spanContext().spanId;
-            } else {
-              object.traceId = 'no-trace-id';
-              object.spanId = 'no-span-id';
-            }
-            return object;
-          },
-    }
-},
-  new pinoOpenObserve({
-    url: "",
-    organization: "logs_traces_correlation",
-    auth: {
-      username: "",
-      password: "",
+  level: 'info',
+  formatters: {
+    log: (object) => {
+      const spanContext = trace.getSpan(context.active());
+      if (spanContext) {
+        object.traceId = spanContext.spanContext().traceId;
+        object.spanId = spanContext.spanContext().spanId;
+      } else {
+        object.traceId = 'no-trace-id';
+        object.spanId = 'no-span-id';
+      }
+      return object;
     },
-    streamName: "logs_traces_correlation",
-    batchSize: 1,
-    timeThreshold: 15 * 1000
-  }),
-);
-
+  },
+  transport: {
+    target: OpenobserveTransport,
+    options: {
+      url: 'https://alpha1.dev.zinclabs.dev/',
+      organization: 'default',
+      streamName: 'logs_traces_correlation',
+      auth: {
+        username: 'omkar@openobserve.ai',
+        password: 'psKcmw4NRv6uiT0s',
+      },
+    },
+  },
+});
 // Export the logger
 module.exports = { logger };
--- tracing.js
+++ tracing.js
@@ -1,40 +1,31 @@
-/* tracing.js */
-const opentelemetry = require("@opentelemetry/sdk-node");
+/*tracking.js*/
+const opentelemetry = require('@opentelemetry/sdk-node');
 const {
   getNodeAutoInstrumentations,
-} = require("@opentelemetry/auto-instrumentations-node");
-const { diag, DiagConsoleLogger, DiagLogLevel } = require("@opentelemetry/api");
+} = require('@opentelemetry/auto-instrumentations-node');
 const {
-  ConsoleSpanExporter,
-  BatchSpanProcessor
-} = require("@opentelemetry/sdk-trace-base");
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-
-const { Resource } = require("@opentelemetry/resources");
-const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-
-// For troubleshooting, set the log level to DiagLogLevel.INFO
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
-
-const traceExporter = new OTLPTraceExporter({
-  url: "http://localhost:5080/api/logs_traces_correlation/traces",
-  headers: {
-    Authorization: "",
-    "stream-name": "logs_traces_correlation",
-  },
-  concurrentExports: 10,  // Increase this to handle more concurrent requests
-});
+  OTLPTraceExporter,
+} = require('@opentelemetry/exporter-trace-otlp-proto');
+// const {
+//   OTLPMetricExporter,
+// } = require('@opentelemetry/exporter-metrics-otlp-proto');
+// const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter,
-  instrumentations: [getNodeAutoInstrumentations()],
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "nodejs-javascript-service",
+  traceExporter: new OTLPTraceExporter({
+    url: "https://alpha1.dev.zinclabs.dev/api/default",
+    headers: {
+      Authorization: "Basic b21rYXJAb3Blbm9ic2VydmUuYWk6cHNLY213NE5SdjZ1aVQwcw==",
+      "stream-name": "logs_traces_correlation",
+    },
   }),
-  spanProcessor: new BatchSpanProcessor(traceExporter), // Ensure you are using the SimpleSpanProcessor
+  // metricReader: new PeriodicExportingMetricReader({
+  //   exporter: new OTLPMetricExporter({
+  //     url: '<your-otlp-endpoint>/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+  //     headers: {}, // an optional object containing custom headers to be sent with each request
+  //     concurrencyLimit: 1, // an optional limit on pending requests
+  //   }),
+  // }),
+  instrumentations: [getNodeAutoInstrumentations()],
 });
-
-// Start the SDK
-sdk.start()
-
-module.exports = sdk;
+sdk.start();
